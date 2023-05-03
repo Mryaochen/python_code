c7606034ff7e68c1e1661fb34a6d9ff48640d868
DarkPlanet <942704802@qq.com>
2023-05-03 15:57:42 +0800

    初识scrapy

---
+++ firstBlood/firstBlood/spiders/first.py
@@ -0,0 +1,15 @@
+import scrapy
+
+
+class FirstSpider(scrapy.Spider):
+    #爬虫文件的名称：爬虫源文件的一个唯一标识
+    name = "first"
+    #允许的域名：用来限定start_urls列表中哪些url可以进行请求发送，通常会注释掉 -- 小猿圈
+    # allowed_domains = ["www.xxx.com"]
+
+    #起始的url列表：该列表中存放的url会被scrapy自动请求的发送
+    start_urls = ["https://www.baidu.com/","https://www.sogou.com/"]
+
+    #用作于数据解析 response表示的就是请求成功后对应的响应对象
+    def parse(self, response):
+        print(response)
